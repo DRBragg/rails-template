ec6e8bbd6508aebcbdf129fd0bfdbf70f402c756
Drew Bragg <DRBragg@gmail.com>
2018-10-24 16:09:30 -0400

    changed repo in template

--- template.rb
+++ template.rb
@@ -82,7 +82,7 @@ def add_template_repository_to_source_path
     at_exit { FileUtils.remove_entry(tempdir) }
     git clone: [
       "--quiet",
-      "https://github.com/mattbrictson/rails-template.git",
+      "https://github.com/DRBragg/rails-template.git",
       tempdir
     ].map(&:shellescape).join(" ")
 
